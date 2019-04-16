f3c8ba13c5377a157cb9c2791a36d51311521202
yangentao <entaoyang@163.com>
2019-04-17 06:40:30 +0800

    2.0.5

--- uilib/src/main/java/dev/entao/ui/base/UIBaseUtils.kt
+++ uilib/src/main/java/dev/entao/ui/base/UIBaseUtils.kt
@@ -1,24 +1,27 @@
+@file:Suppress("unused")
+
 package dev.entao.ui.base
 
 import android.support.v4.app.Fragment
 
 
 val Fragment.containerActivity: ContainerActivity get() = this.act as ContainerActivity
+val Fragment.containerAct: ContainerActivity? get() = this.activity as? ContainerActivity
 
 
 fun BaseFragment.pushPage(p: BaseFragment, pushAnim: Boolean, popAnim: Boolean) {
-    this.containerActivity.push(p, pushAnim, popAnim)
+    this.containerAct?.push(p, pushAnim, popAnim)
 }
 
 fun BaseFragment.pushPage(p: BaseFragment) {
-    this.containerActivity.push(p)
+    this.containerAct?.push(p)
 }
 
 fun BaseFragment.pushPage(p: BaseFragment, block: BaseFragment.() -> Unit) {
     p.block()
-    this.containerActivity.push(p)
+    this.containerAct?.push(p)
 }
 
 fun BaseFragment.popPage() {
-    this.containerActivity.pop(this)
+    this.containerAct?.pop(this)
 }
