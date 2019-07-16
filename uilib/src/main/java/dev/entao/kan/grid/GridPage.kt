29fe59860920f5e087c1028a78f5ea20c4b153f5
yangentao <entaoyang@163.com>
2019-07-16 23:20:46 +0800

    4.0.12

--- uilib/src/main/java/dev/entao/kan/grid/GridPage.kt
+++ uilib/src/main/java/dev/entao/kan/grid/GridPage.kt
@@ -4,9 +4,14 @@ import android.content.Context
 import android.view.View
 import android.widget.AdapterView
 import android.widget.LinearLayout
-import dev.entao.kan.ext.HeightFlex
-import dev.entao.kan.ext.LParam
-import dev.entao.kan.ext.WidthFill
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import dev.entao.kan.appbase.Task
+import dev.entao.kan.creator.relative
+import dev.entao.kan.creator.swipeRefresh
+import dev.entao.kan.creator.textView
+import dev.entao.kan.ext.*
 import dev.entao.kan.list.AnyAdapter
 import dev.entao.kan.page.TitlePage
 
@@ -15,8 +20,17 @@ import dev.entao.kan.page.TitlePage
  */
 
 abstract class GridPage : TitlePage() {
-    lateinit var gridView: LineGridView
     var anyAdapter: AnyAdapter = AnyAdapter()
+    lateinit var gridView: LineGridView
+    lateinit var gridParent: RelativeLayout
+        private set
+    lateinit var refreshLayout: SwipeRefreshLayout
+        private set
+    lateinit var emptyView: TextView
+        private set
+
+    var emptyText = "没有内容"
+    var loadingText = "加载中..."
 
     override fun onCreateContent(context: Context, contentView: LinearLayout) {
         gridView = LineGridView(context)
@@ -28,16 +42,55 @@ abstract class GridPage : TitlePage() {
         }
         anyAdapter.onNewView = this::onNewItemView
         anyAdapter.onRequestItems = this::onRequestItems
+        anyAdapter.onItemsRefreshed = this::onItemsRefreshed
 
         gridView.adapter = anyAdapter
         gridView.onItemClickListener = AdapterView.OnItemClickListener { _, _, pos, _ ->
             onItemClick(anyAdapter.getItem(pos))
         }
-        onLayoutGridView(gridView)
+        gridParent = contentView.relative(LParam.WidthFill.HeightFlex) {
+            refreshLayout = swipeRefresh(RParam.Fill) {
+                isEnabled = false
+                setOnRefreshListener {
+                    onPullRefresh()
+                }
+                setColorSchemeResources(
+                    android.R.color.holo_green_dark,
+                    android.R.color.holo_blue_dark,
+                    android.R.color.holo_orange_dark,
+                    android.R.color.holo_red_dark
+                )
+                addView(gridView, MParam.Fill)
+            }
+            emptyView = textView(RParam.Fill) {
+                textS = emptyText
+                gravityCenter().textSizeB().gone()
+            }
+            gridView.emptyView = emptyView
+        }
     }
 
-    open fun onLayoutGridView(gridView: LineGridView) {
-        contentView.addView(gridView, LParam.WidthFill.HeightFlex)
+    protected open fun onItemsRefreshed() {
+        setRefreshing(false)
+    }
+
+    fun enablePullRefresh(enable: Boolean = true) {
+        refreshLayout.isEnabled = enable
+    }
+
+    fun setRefreshing(refresh: Boolean) {
+        Task.mainThread {
+            refreshLayout.isRefreshing = refresh
+            if (refresh) {
+                emptyView.text = loadingText
+            } else {
+                emptyView.text = emptyText
+            }
+        }
+    }
+
+    open fun onPullRefresh() {
+        requestItems()
     }
 
     open fun setItems(items: List<Any>) {
@@ -55,6 +108,9 @@ abstract class GridPage : TitlePage() {
 
     fun requestItems() {
         anyAdapter.requestItems()
+        if (refreshLayout.isEnabled) {
+            setRefreshing(true)
+        }
     }
 
     open fun onRequestItems(): List<Any> {
