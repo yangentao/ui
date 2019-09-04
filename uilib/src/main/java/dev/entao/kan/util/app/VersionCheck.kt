cc6545f68f886ffca7369071c62884120c62a445
yangentao <entaoyang@163.com>
2019-09-04 15:34:11 +0800

    4.0.22

---
+++ uilib/src/main/java/dev/entao/kan/util/app/VersionCheck.kt
@@ -0,0 +1,126 @@
+@file:Suppress("unused")
+
+package dev.entao.kan.util.app
+
+import android.app.Activity
+import androidx.annotation.Keep
+import dev.entao.kan.appbase.App
+import dev.entao.kan.appbase.Task
+import dev.entao.kan.appbase.sql.MapTable
+import dev.entao.kan.base.BasePage
+import dev.entao.kan.base.Progress
+import dev.entao.kan.base.act
+import dev.entao.kan.base.openActivity
+import dev.entao.kan.dialogs.DialogX
+import dev.entao.kan.http.Http
+import dev.entao.kan.json.YsonObject
+import dev.entao.kan.util.ToastUtil
+import java.io.File
+
+/**
+ * Created by entaoyang@163.com on 2017-06-13.
+ */
+
+@Keep
+class VersionCheck(val jo: YsonObject) {
+    val versionCode: Int by jo
+    val versionName: String by jo
+    val msg: String by jo
+    val resId: Int by jo
+    val download: String by jo
+
+    fun great(): Boolean {
+        return versionCode > App.versionCode
+    }
+
+    fun downloadFile(progress: Progress?): File? {
+        val f = App.files.ex.temp(App.appName + ".apk")
+        val r = Http(this.download).download(f, progress)
+        if (r.OK) {
+            return f
+        }
+        return null
+    }
+
+    companion object {
+        val CHECK_URL: String = "http://app800.cn/am/check"
+        var CHECK_HOURS = 4 //最多每4小时检查一次
+
+        private val ignoreMap = MapTable("ver_ignore")
+
+        private var lastCheckUpdate: Long by MapTable.config
+
+        private fun isIgnored(verCode: Int): Boolean {
+            return ignoreMap[verCode.toString()] != null
+        }
+
+        fun checkByUser(page: BasePage) {
+            page.openActivity(VersionUpgradeActivity::class)
+        }
+
+        fun checkAuto(page: BasePage) {
+            val last = lastCheckUpdate
+            val now = System.currentTimeMillis()
+            if (now - last < CHECK_HOURS * 60 * 60 * 1000) {
+                return
+            }
+            Task.back {
+                val v = check()
+                Task.fore {
+                    if (v != null) {
+                        lastCheckUpdate = now
+                        if (!isIgnored(v.versionCode)) {
+                            if (page.activity != null) {
+                                confirmInstall(page.act, v, true)
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+        private fun confirmInstall(act: Activity, v: VersionCheck, quiet: Boolean) {
+            if (!v.great()) {
+                try {
+                    if (!quiet) {
+                        ToastUtil.showLong(act, "已经是最新版本")
+                    }
+                } catch (ex: Exception) {
+                }
+                return
+            }
+
+            DialogX(act).apply {
+                title("检查升级")
+                bodyText("发现新版本${v.versionName}")
+                buttons {
+                    ok("升级") {
+                        act.openActivity(VersionUpgradeActivity::class)
+                    }
+                    normal("忽略此版本") {
+                        ignoreMap.put(v.versionCode.toString(), v.versionName)
+                    }
+                    cancel("取消")
+                }
+                show()
+            }
+        }
+
+
+        fun check(): VersionCheck? {
+            val r = Http(CHECK_URL).arg("pkg", App.packageName).get()
+            if (r.OK) {
+                val jo = r.ysonObject() ?: return null
+                if (jo.int("code") != 0) {
+                    return null
+                }
+                val jdata = jo.obj("data") ?: return null
+                return VersionCheck(jdata)
+            }
+            return null
+        }
+
+
+    }
+}
---
+++ uilib/src/main/java/dev/entao/kan/util/app/VersionUpgradeActivity.kt
@@ -0,0 +1,193 @@
+package dev.entao.kan.util.app
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.provider.Settings
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import dev.entao.kan.appbase.App
+import dev.entao.kan.appbase.Task
+import dev.entao.kan.base.Progress
+import dev.entao.kan.base.TitledActivity
+import dev.entao.kan.base.ex.keepDot
+import dev.entao.kan.creator.buttonGreenRound
+import dev.entao.kan.creator.textView
+import dev.entao.kan.ext.*
+import dev.entao.kan.log.loge
+import java.io.File
+
+
+class VersionUpgradeActivity : TitledActivity(), Progress {
+
+    lateinit var msgView: TextView
+    lateinit var upButton: Button
+    lateinit var webButton: Button
+    var ver: VersionCheck? = null
+    var apkFile: File? = null
+
+    override fun onCreateContent(contentView: LinearLayout) {
+        titleBar.title("升级")
+        titleBar.rightText("检查").onClick = {
+            queryVersion()
+        }
+        titleBar.showBack().onClick = {
+            finish()
+        }
+
+        msgView = contentView.textView(LParam.WidthFill.height(80).margins(20)) {
+            textSizeSp(20)
+            gravityCenter()
+            textColorMajor()
+            text = "正在检查是否有新版本..."
+        }
+        upButton = contentView.buttonGreenRound {
+            text = "升级"
+            gone()
+            onClick {
+                clickUp()
+            }
+        }
+
+        webButton = contentView.buttonGreenRound {
+            text = "用浏览器打开"
+            gone()
+            onClick {
+                openWeb()
+            }
+        }
+        queryVersion()
+    }
+
+    private fun openWeb() {
+        val url = ver?.download ?: return
+        App.openUrl(url)
+    }
+
+    private fun sizeText(n: Int): String {
+        return when {
+            n > 1000_000 -> (n.toDouble() / 1000_000).keepDot(2) + "M"
+            n > 1000 -> (n.toDouble() / 1000).keepDot(2) + "K"
+            else -> "$n 字节"
+        }
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onProgressStart(total: Int) {
+        Task.fore {
+            msgView.text = "正在下载:共${sizeText(total)}"
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onProgress(current: Int, total: Int, percent: Int) {
+        Task.fore {
+            msgView.text = "正在下载:共${sizeText(total)}, $percent%"
+        }
+    }
+
+    override fun onProgressFinish() {
+        Task.fore {
+            msgView.text = "下载完成"
+        }
+    }
+
+    private fun download() {
+        val oldFile = apkFile
+        if (oldFile != null) {
+            Task.fore {
+                install(oldFile)
+            }
+            return
+        }
+
+        val v = ver ?: return
+        Task.back {
+            val f = v.downloadFile(this@VersionUpgradeActivity)
+            Task.fore {
+                if (f != null) {
+                    apkFile = f
+                    install(f)
+                }
+            }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun install(file: File) {
+        val apkUri = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            Uri.fromFile(file)
+        } else {
+            FileProv.uriOfFile(file)
+        }
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        }
+        intent.setDataAndType(apkUri, "application/vnd.android.package-archive")
+        val ls = packageManager.queryIntentActivities(intent, 0)
+        if (ls.isEmpty()) {
+            msgView.text = "未找到APK安装程序"
+            return
+        }
+        try {
+            startActivity(intent)
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+            loge(ex)
+        }
+    }
+
+    private fun clickUp() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val hasInstallPermission = packageManager.canRequestPackageInstalls()
+            if (!hasInstallPermission) {
+                val intent = Intent(Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES, Uri.parse("package:$packageName"))
+                startActivityForResult(intent, 1)
+                return
+            }
+        }
+        download()
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 1) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                if (packageManager.canRequestPackageInstalls()) {
+                    download()
+                } else {
+                    msgView.text = "没有得到安装apk文件的授权"
+                }
+            }
+            return
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun queryVersion() {
+        Task.back {
+            val v = VersionCheck.check()
+            Task.fore {
+                if (v == null || !v.great()) {
+                    msgView.text = "已经是最新版本"
+                } else {
+                    val m = if (v.msg != v.versionName) {
+                        v.msg
+                    } else {
+                        ""
+                    }
+                    msgView.text = "发现新版本: ${v.versionName} $m"
+                    upButton.visiable()
+                    webButton.visiable()
+                    ver = v
+                }
+            }
+        }
+    }
+}
