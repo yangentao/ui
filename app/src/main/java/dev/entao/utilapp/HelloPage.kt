6efc91c700e29f15739db58f5f69d483e1740588
yangentao <entaoyang@163.com>
2019-09-26 01:17:38 +0800

    21

--- app/src/main/java/dev/entao/utilapp/HelloPage.kt
+++ app/src/main/java/dev/entao/utilapp/HelloPage.kt
@@ -2,11 +2,14 @@ package dev.entao.utilapp
 
 import android.content.Context
 import android.widget.LinearLayout
+import dev.entao.kan.appbase.ex.Colors
+import dev.entao.kan.appbase.ex.Shapes
+import dev.entao.kan.appbase.ex.dp
+import dev.entao.kan.creator.button
+import dev.entao.kan.creator.buttonRedRound
 import dev.entao.kan.creator.edit
-import dev.entao.kan.ext.HeightEdit
-import dev.entao.kan.ext.LParam
-import dev.entao.kan.ext.WidthFill
-import dev.entao.kan.ext.imeDone
+import dev.entao.kan.creator.textView
+import dev.entao.kan.ext.*
 import dev.entao.kan.page.TitlePage
 import dev.entao.kan.res.Res
 
@@ -18,9 +21,35 @@ class HelloPage : TitlePage() {
             title("Hello")
             rightImage(Res.addWhite)
         }
-
-        contentView.edit (LParam.WidthFill.HeightEdit){
+        contentView.backColor(Colors.WHITE)
+        contentView.edit(LParam.WidthFill.HeightEdit) {
             this.imeDone()
+        }
+        contentView.buttonRedRound {
+            text = "Hello"
+            this.elevation = 3.0f
+            this.translationZ = 5f
+        }
+
+        contentView.button(LParam.width(80).HeightButton.gravityCenter().marginTop(30)) {
+            text = "Yang"
+            this.backColor(Colors.WHITE)
+            this.elevation = 50.0f
+        }
+        contentView.textView(LParam.width(100).HeightButton.gravityCenter().marginTop(30)) {
+            text = "Yang"
+            this.background = Shapes.rect {
+                cornerDp = 3
+                fillColor = Colors.RedMajor
+            }
+            this.elevation = 30.0f
+            this.translationZ = 5f
+        }
+        contentView.textView(LParam.width(100).HeightButton.gravityCenter().marginTop(30)) {
+            text = "Yang"
+            this.backColorWhite()
+            this.elevation = 10.dp.toFloat()
+            this.translationZ = 0f
         }
     }
 
