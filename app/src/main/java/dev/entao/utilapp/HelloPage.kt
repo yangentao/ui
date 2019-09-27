bed7c53af6257d06a4b5b05b9a14c9c779d4bf4c
yangentao <entaoyang@163.com>
2019-09-27 17:50:28 +0800

    4.1.3

--- app/src/main/java/dev/entao/utilapp/HelloPage.kt
+++ app/src/main/java/dev/entao/utilapp/HelloPage.kt
@@ -1,9 +1,19 @@
 package dev.entao.utilapp
 
 import android.content.Context
+import android.graphics.Color
+import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.slidingpanelayout.widget.SlidingPaneLayout
+import dev.entao.kan.appbase.ex.Colors
+import dev.entao.kan.appbase.ex.dp
 import dev.entao.kan.base.ManiPerm
 import dev.entao.kan.base.reqPerm
+import dev.entao.kan.creator.createTextView
+import dev.entao.kan.ext.LParam
+import dev.entao.kan.ext.WidthFill
+import dev.entao.kan.ext.backColor
+import dev.entao.kan.ext.height
 import dev.entao.kan.log.logd
 import dev.entao.kan.page.TitlePage
 import dev.entao.kan.util.app.ApkDownReceiver
@@ -18,6 +28,20 @@ class HelloPage : TitlePage() {
                 down()
             }
         }
+
+        val a = SlidingPaneLayout(context)
+        contentView.addView(a, LParam.WidthFill.height(200))
+        val b = createTextView()
+        val c = createTextView()
+//        val d = createTextView()
+        a.addView(b, 100.dp, ViewGroup.LayoutParams.MATCH_PARENT)
+        a.addView(c, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+//        a.addView(d, 150.dp, ViewGroup.LayoutParams.MATCH_PARENT)
+
+        a.backColor(Color.DKGRAY)
+        b.backColor(Colors.RedMajor)
+        c.backColor(Colors.GreenMajor)
+//        d.backColor(Colors.BlueMajor)
 
     }
 
