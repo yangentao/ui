b5ecebcca32955b57c40705920baca8cbb4b840b
yangentao <entaoyang@163.com>
2019-10-01 17:52:58 +0800

    4121

--- uilib/src/main/java/dev/entao/kan/base/UiUtils.kt
+++ uilib/src/main/java/dev/entao/kan/base/UiUtils.kt
@@ -1,2 +1,19 @@
 package dev.entao.kan.base
 
+import dev.entao.kan.json.YsonArray
+import dev.entao.kan.json.YsonObject
+
+
+object YsonObjectTextConvert : ITextConvert {
+    override val defaultValue: Any = YsonObject()
+    override fun fromText(text: String): Any? {
+        return YsonObject(text)
+    }
+}
+
+object YsonArrayTextConvert : ITextConvert {
+    override val defaultValue: Any = YsonArray()
+    override fun fromText(text: String): Any? {
+        return YsonArray(text)
+    }
+}
